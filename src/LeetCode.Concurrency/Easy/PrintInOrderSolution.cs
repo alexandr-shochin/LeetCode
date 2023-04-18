e67990142e07f30392f45fa7424bf9023ec8d898
Александр Шочин <alexandr.shochin@gmail.com>
2023-04-18 13:04:17 +0300

    Concurrency. Easy. 1114. Print in Order - Solved

---
+++ src/LeetCode.Concurrency/Easy/PrintInOrderSolution.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Concurrency.Easy;
+
+/// <summary>1114. Print in Order</summary>
+/// <remarks>https://leetcode.com/problems/print-in-order/</remarks>>
+public sealed class PrintInOrderSolution
+{
+    private readonly ManualResetEventSlim _firstManualResetEventSlim = new(false);
+    private readonly ManualResetEventSlim _secondManualResetEventSlim = new(false);
+    
+    public PrintInOrderSolution()
+    {
+    }
+
+    public void First(Action printFirst)
+    {
+        printFirst();
+
+        _firstManualResetEventSlim.Set();
+    }
+
+    public void Second(Action printSecond)
+    {
+        _firstManualResetEventSlim.Wait();
+
+        printSecond();
+
+        _secondManualResetEventSlim.Set();
+    }
+
+    public void Third(Action printThird)
+    {
+        _secondManualResetEventSlim.Wait();
+
+        printThird();
+    }
+}
---
+++ tests/LeetCode.UnitTests/Concurrency/PrintInOrderSolutionTests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Concurrency.Easy;
+using Xunit.Abstractions;
+
+namespace LeetCode.UnitTests.Concurrency;
+
+public sealed class PrintInOrderSolutionTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public PrintInOrderSolutionTests(ITestOutputHelper testOutputHelper) => _testOutputHelper = testOutputHelper;
+
+    [Fact]
+    public void Given_some_set_of_input_parameters_When_call_RomanToInt_Then_should_return_correct_solutions()
+    {
+        // Arrange
+        const int ExperimentCount = 10;
+        const string ExpectedSolution = "firstsecondthird";
+        
+        // Act and Assert
+        for (var experimentNumber = 1; experimentNumber <= ExperimentCount; experimentNumber++)
+        {
+            var alg = new PrintInOrderSolution();
+            
+            var actualSolution = string.Empty;
+            var threads = new[]
+            {
+                new Thread(_ => alg.First(() => actualSolution += "first")),
+                new Thread(_ => alg.Second(() => actualSolution += "second")),
+                new Thread(_ => alg.Third(() => actualSolution += "third"))
+            };
+            
+            Parallel.ForEach(threads, thread => thread.Start());
+            Thread.Sleep(1 * 1000);
+        
+            actualSolution.Should().Be(ExpectedSolution);
+            _testOutputHelper.WriteLine($"experimentNumber: {experimentNumber} OK!");
+        }
+    }
+}
