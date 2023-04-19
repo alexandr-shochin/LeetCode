c2cbc11f711df15e6f7fd43167ea32000960b86d
Александр Шочин <alexandr.shochin@gmail.com>
2023-04-19 18:41:09 +0300

    Algorithms. Easy. 21. Merge Two Sorted Lists - Solved

---
+++ src/LeetCode.Algorithms/Easy/ValidParenthesesSolution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Algorithms.Easy;
+
+/// <summary>20. Valid Parentheses</summary>
+/// <remarks>https://leetcode.com/problems/valid-parentheses/</remarks>
+public sealed class ValidParenthesesSolution
+{
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        foreach (var ch in s)
+        {
+            if (ch is '(' or '{' or '[')
+                stack.Push(ch);
+            else
+            {
+                if (stack is { Count: 0 })
+                    return false;
+
+                var peekedChar = stack.Peek();
+                switch (ch)
+                {
+                    case ')' when peekedChar is not '(':
+                    case '}' when peekedChar is not '{':
+                    case ']' when peekedChar is not '[':
+                        return false;
+                    default:
+                        stack.Pop();
+                        continue;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
---
+++ tests/LeetCode.UnitTests/Algorithms/Easy/ValidParenthesesSolutionTests.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.UnitTests.Algorithms.Easy;
+
+public sealed class ValidParenthesesSolutionTests
+{
+    [Theory]
+    [InlineData("()", true)]
+    [InlineData("()[]{}", true)]
+    [InlineData("(]", false)]
+    [InlineData("", true)]
+    [InlineData("()[", false)]
+    [InlineData("{[]}", true)]
+    [InlineData("{{{{(((([[[[]]]]))))}}}}", true)]
+    [InlineData("([}}])", false)]
+    [InlineData("([)]", false)] 
+    [InlineData("((", false)]
+    [InlineData("))", false)]
+    public void Given_some_set_of_input_parameters_When_call_IsValid_Then_should_return_correct_solutions(string s, bool expectedSolution)
+    {
+        // Arrange
+        var alg = new ValidParenthesesSolution();
+
+        // Act
+        var actualSolution = alg.IsValid(s);
+
+        // Assert
+        actualSolution.Should().Be(expectedSolution);
+    }
+}
