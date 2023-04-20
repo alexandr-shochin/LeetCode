c58caad1aa7bb7aab3ac25d3a6423746272951ca
Александр Шочин <alexandr.shochin@gmail.com>
2023-04-20 14:41:20 +0300

    Algorithms. Easy. 28. Find the Index of the First Occurrence in a String - Solved

---
+++ src/LeetCode.Algorithms/Easy/FindTheIndexOfTheFirstOccurrenceInAStringSolution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Algorithms.Easy;
+
+/// <summary>28. Find the Index of the First Occurrence in a String</summary>
+/// <remarks>https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/</remarks>
+public sealed class FindTheIndexOfTheFirstOccurrenceInAStringSolution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        if (string.IsNullOrEmpty(haystack) ||
+            string.IsNullOrEmpty(needle) ||
+            haystack.Length < needle.Length)
+            return -1;
+        
+        for (var haystackIndex = 0; haystackIndex < haystack.Length; haystackIndex++)
+        {
+            var tmpIndex = haystackIndex;
+            for (var needleIndex = 0; needleIndex < needle.Length && tmpIndex < haystack.Length ; needleIndex++)
+            {
+                var needleChar = needle[needleIndex];
+                if (haystack[tmpIndex] == needleChar)
+                    tmpIndex++;
+                else
+                    break;
+            }
+
+            if (tmpIndex - haystackIndex == needle.Length)
+                return haystackIndex;
+        }
+
+        return -1;
+    }
+}
---
+++ tests/LeetCode.UnitTests/Algorithms/Easy/FindTheIndexOfTheFirstOccurrenceInAStringSolutionTests.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.UnitTests.Algorithms.Easy;
+
+public sealed class FindTheIndexOfTheFirstOccurrenceInAStringSolutionTests
+{
+    [Theory]
+    [InlineData("aaasadbutsad", "sad", 3)]
+    [InlineData("sadbutsad", "sad", 0)]
+    [InlineData("leetcode", "leeto", -1)]
+    [InlineData("leetcode", "", -1)]
+    [InlineData("", "leeto", -1)]
+    [InlineData("mississippi", "issip", 4)]
+    [InlineData("aaa", "aaaa", -1)]
+    [InlineData("mississippi", "issipi", -1)]
+    public void Given_some_set_of_input_parameters_When_call_IsValid_Then_should_return_correct_solutions(string haystack, string needle, int expectedSolution)
+    {
+        // Arrange
+        var alg = new FindTheIndexOfTheFirstOccurrenceInAStringSolution();
+
+        // Act
+        var actualSolution = alg.StrStr(haystack, needle);
+
+        // Assert
+        actualSolution.Should().Be(expectedSolution);
+    }
+}
