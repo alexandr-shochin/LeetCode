1bf47691f2ff3e1891fa006fed39a0a4ffdd4fd7
Александр Шочин <alexandr.shochin@gmail.com>
2023-04-20 11:04:32 +0300

    Algorithms. Easy. 26. Remove Duplicates from Sorted Array - Solved

---
+++ src/LeetCode.Algorithms/Easy/RemoveDuplicatesFromSortedArraySolution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms.Easy;
+
+/// <summary>26. Remove Duplicates from Sorted Array</summary>
+/// <remarks>https://leetcode.com/problems/remove-duplicates-from-sorted-array/</remarks>
+public sealed class RemoveDuplicatesFromSortedArraySolution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var tmpIndex = 1;
+
+        for (var numIndex = 0; numIndex < nums.Length; numIndex++)
+        {
+            if (nums[numIndex] == nums[tmpIndex - 1])
+                continue;
+
+            nums[tmpIndex] = nums[numIndex];
+            tmpIndex++;
+        }
+
+        return tmpIndex;
+    }
+}
---
+++ tests/LeetCode.UnitTests/Algorithms/Easy/RemoveDuplicatesFromSortedArraySolutionTests.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.UnitTests.Algorithms.Easy;
+
+public sealed class RemoveDuplicatesFromSortedArraySolutionTests
+{
+    [Theory]
+    [InlineData(new [] { 1, 1, 2 }, 2, new [] { 1, 2 })]
+    [InlineData(new [] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5, new [] { 0, 1, 2, 3, 4 })]
+    [InlineData(new [] { 1, 1, 2, 2, 3, 3 }, 3, new [] { 1, 2, 3 })]
+    public void Given_some_set_of_input_parameters_When_call_RemoveDuplicates_Then_should_return_correct_solutions(
+        int[] nums, int expectedK, int[] expectedNums)
+    {
+        // Arrange
+        var alg = new RemoveDuplicatesFromSortedArraySolution();
+
+        // Act
+        var actualSolution = alg.RemoveDuplicates(nums);
+
+        // Assert
+        actualSolution.Should().Be(expectedK);
+
+        for (var expectedIndex = 0; expectedIndex < expectedK; expectedIndex++)
+            nums[expectedIndex].Should().Be(expectedNums[expectedIndex]);
+    }
+}
