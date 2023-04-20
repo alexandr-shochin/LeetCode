c1b748d2502e3f61624d1793b9179fd3813d114d
Александр Шочин <alexandr.shochin@gmail.com>
2023-04-20 13:00:29 +0300

    Algorithms. Easy. 27. Remove Element - Solved

---
+++ src/LeetCode.Algorithms/Easy/RemoveElementSolution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms.Easy;
+
+/// <summary>27. Remove Element</summary>
+/// <remarks>https://leetcode.com/problems/remove-element/</remarks>
+public sealed class RemoveElementSolution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var tmpIndex = 0;
+        
+        for (var numIndex = 0; numIndex < nums.Length; numIndex++)
+        {
+            if (nums[numIndex] == val)
+                continue;
+            
+            nums[tmpIndex] = nums[numIndex];
+            tmpIndex++;
+        }
+
+        return tmpIndex;
+    }
+}
---
+++ tests/LeetCode.UnitTests/Algorithms/Easy/RemoveElementSolutionTests.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.UnitTests.Algorithms.Easy;
+
+public sealed class RemoveElementSolutionTests
+{
+    [Theory]
+    [InlineData(new [] { 3, 2, 2, 3 }, 3, 2, new [] { 2, 2 })]
+    [InlineData(new [] { 3, 3, 2, 3 }, 3, 1, new [] { 2 })]
+    [InlineData(new [] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5, new [] { 0, 0, 1, 3, 4 })]
+    [InlineData(new [] { 1, 1, 2, 2, 3, 2, 4, 1 }, 2, 5, new [] { 1, 1, 1, 3, 4 })]
+    public void Given_some_set_of_input_parameters_When_call_RemoveDuplicates_Then_should_return_correct_solutions(
+        int[] nums, int val, int expectedK, int[] expectedNums)
+    {
+        // Arrange
+        var alg = new RemoveElementSolution();
+
+        // Act
+        var actualSolution = alg.RemoveElement(nums, val);
+
+        // Assert
+        actualSolution.Should().Be(expectedK);
+        Array.Sort(nums, 0, actualSolution);
+        for (var expectedIndex = 0; expectedIndex < expectedK; expectedIndex++)
+            nums[expectedIndex].Should().Be(expectedNums[expectedIndex]);
+    }
+}
