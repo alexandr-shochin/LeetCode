171c2f5d0583f10df40e14dc17748b40f303f7bf
Александр Шочин <alexandr.shochin@gmail.com>
2023-04-21 17:18:34 +0300

    Algorithms. Easy. 111. Minimum Depth of Binary Tree - Solved

---
+++ src/LeetCode.Algorithms/Easy/Tree/MinimumDepthOfBinaryTreeSolution.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.Algorithms.Easy.Tree;
+
+/// <summary>111. Minimum Depth of Binary Tree</summary>
+/// <remarks>https://leetcode.com/problems/minimum-depth-of-binary-tree/</remarks>>
+public sealed class MinimumDepthOfBinaryTreeSolution
+{
+    private readonly Queue<(TreeNode, int)> _queue = new();
+    private int _minDepth = int.MaxValue;
+    
+    public int MinDepth(TreeNode? root)
+    {
+        if (root is null)
+            return 0;
+
+        if (root.left is null && root.right is null)
+            return 1;
+
+        AddAdjacentNodes(root, 1);
+
+        while (_queue is not { Count: 0 })
+        {
+            var (currentNode, depth) = _queue.Dequeue();
+
+            if (IsLeaf(currentNode))
+            {
+                _minDepth = depth;
+                break;
+            }
+
+            AddAdjacentNodes(currentNode, depth);
+        }
+
+        return _minDepth;
+    }
+
+    private void AddAdjacentNodes(TreeNode node, int depth)
+    {
+        AddNodeToStack(node.left, depth);
+        AddNodeToStack(node.right, depth);
+    }
+
+    private void AddNodeToStack(TreeNode? node, int prevDepth)
+    {
+        if (node is null)
+            return;
+        
+        _queue.Enqueue((node, prevDepth + 1));
+    }
+
+    private bool IsLeaf(TreeNode node) => node.left is null && node.right is null;
+}
---
+++ tests/LeetCode.UnitTests/Algorithms/Easy/Tree/MinimumDepthOfBinaryTreeSolutionTests.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.UnitTests.Algorithms.Easy.Tree;
+
+public sealed class MinimumDepthOfBinaryTreeSolutionTests
+{
+    [Fact]
+    public void Given_some_set_of_input_parameters_When_call_MinDepth_Then_should_return_correct_solutions()
+    {
+        // Arrange
+        var alg = new MinimumDepthOfBinaryTreeSolution();
+
+        // Act
+        var tree = new TreeNode(
+            3,
+            left: new TreeNode(9,
+                left: new TreeNode(8)),
+            right: new TreeNode(
+                20,
+                left: new TreeNode(15),
+                right: new TreeNode(7,
+                    left: new TreeNode(2),
+                    right: new TreeNode(5))));
+        var actualSolution = alg.MinDepth(tree);
+
+        // Assert
+        actualSolution.Should().Be(3);
+    }
+}
