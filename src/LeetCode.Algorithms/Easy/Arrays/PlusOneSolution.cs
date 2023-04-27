9396f7ce6b70dfdbf2c62425dd04a2e1e2574944
Александр Шочин <alexandr.shochin@gmail.com>
2023-04-27 12:24:53 +0300

    Algorithms. Easy. 66. Plus One - Solved

---
+++ src/LeetCode.Algorithms/Easy/Arrays/PlusOneSolution.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Algorithms.Easy.Arrays;
+
+/// <summary>66. Plus One</summary>
+/// <remarks>https://leetcode.com/problems/plus-one/</remarks>
+public sealed class PlusOneSolution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        var digitsLength = digits.Length;
+
+        var tmpArrayLength = digitsLength + 1;
+        var tmpArray = new int[tmpArrayLength];
+        
+        var inMemory = 1;
+        for (var tmpArrayIndex = tmpArrayLength - 1; tmpArrayIndex >= 0; tmpArrayIndex--)
+        {
+            var digit = 0;
+            if (tmpArrayIndex - 1 >= 0)
+                digit = digits[tmpArrayIndex - 1];
+
+            var sum = digit + inMemory;
+            inMemory = sum / 10;
+            
+            tmpArray[tmpArrayIndex] = sum % 10;
+        }
+        
+        int resultArrayLength;
+        int sourceIndex;
+        if (tmpArray.First() is 0)
+        {
+            resultArrayLength = digitsLength;
+            sourceIndex = 1;
+        }
+        else
+        {
+            resultArrayLength = tmpArrayLength;
+            sourceIndex = 0;
+        }
+
+        var resultArray = new int[resultArrayLength];
+        Array.Copy(tmpArray, sourceIndex, resultArray, 0, resultArrayLength);
+        return resultArray;
+    }
+}
---
+++ tests/LeetCode.UnitTests/Algorithms/Easy/Arrays/PlusOneSolutionTests.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.UnitTests.Algorithms.Easy.Arrays;
+
+public sealed class PlusOneSolutionTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 2, 3 }, new[] { 1, 2, 4 })]
+    [InlineData(new[] { 4, 3, 2, 1 }, new[] { 4, 3, 2, 2 })]
+    [InlineData(new[] { 1, 2, 3, 9 }, new[] { 1, 2, 4, 0 })]
+    [InlineData(new[] { 9 }, new[] { 1, 0 })]
+    public void Given_some_set_of_input_parameters_When_call_PlusOne_Then_should_return_correct_solutions(int[] digits, int[] expectedSolution)
+    {
+        // Arrange
+        var alg = new PlusOneSolution();
+
+        // Act
+        var actualSolution = alg.PlusOne(digits);
+
+        // Assert
+        actualSolution.Should().BeEquivalentTo(expectedSolution);
+    }
+}
