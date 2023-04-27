336bac94913395e5d7390e8cc0bffc5f4bc6833f
Александр Шочин <alexandr.shochin@gmail.com>
2023-04-27 13:33:05 +0300

    Algorithms. Easy. 69. Sqrt(x) - Solved

---
+++ src/LeetCode.Algorithms/Easy/Math/SqrtXSolution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Algorithms.Easy.Math;
+
+/// <summary>69. Sqrt(x)</summary>
+/// <remarks>https://leetcode.com/problems/sqrtx/</remarks>
+public sealed class SqrtXSolution
+{
+    public int MySqrt(int x)
+    {
+        if (x is 1)
+            return 1;
+        
+        double leftBorder = 0;
+        double rightBorder = x;
+        
+        var middle = x / 2.0d;
+        while (leftBorder <= rightBorder)
+        {
+            var doubleMiddle = middle * middle;
+            if (System.Math.Abs(doubleMiddle - x) < 1e-6)
+                return (int)middle;
+            
+            if (doubleMiddle < x)
+                leftBorder = middle;
+            else
+                rightBorder = middle;
+            
+            middle = (leftBorder + rightBorder) / 2;
+        }
+
+        return (int)middle;
+    }
+}
---
+++ tests/LeetCode.UnitTests/Algorithms/Easy/Math/SqrtXSolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Algorithms.Easy.Math;
+
+namespace LeetCode.UnitTests.Algorithms.Easy.Math;
+
+public sealed class SqrtXSolutionTests
+{
+    [Theory]
+    [InlineData(9, 3)]
+    [InlineData(8, 2)]
+    [InlineData(1, 1)]
+    public void Given_some_set_of_input_parameters_When_call_IsValid_Then_should_return_correct_solutions(int x, int expectedSolution)
+    {
+        // Arrange
+        var alg = new SqrtXSolution();
+
+        // Act
+        var actualSolution = alg.MySqrt(x);
+
+        // Assert
+        actualSolution.Should().Be(expectedSolution);
+    }
+}
