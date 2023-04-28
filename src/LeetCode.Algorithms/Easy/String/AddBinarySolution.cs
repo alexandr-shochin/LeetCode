85143d2f20551aaa24d65f5b1841567ddf91b642
Александр Шочин <alexandr.shochin@gmail.com>
2023-04-28 17:50:57 +0300

    Algorithms. Easy. 67. Add Binary - Solved

---
+++ src/LeetCode.Algorithms/Easy/String/AddBinarySolution.cs
@@ -0,0 +1,74 @@
+﻿namespace LeetCode.Algorithms.Easy.String;
+
+/// <summary>67. Add Binary</summary>
+/// <remarks>https://leetcode.com/problems/add-binary/</remarks>
+public sealed class AddBinarySolution
+{
+    public string? AddBinary(string? a, string? b)
+    {
+        if (string.IsNullOrWhiteSpace(a) || string.IsNullOrWhiteSpace(b))
+            return null;
+
+        var result = string.Empty;
+        
+        var (sum, inMemory) = ('0', '0');
+        for (int aIndex = a.Length - 1, bIndex = b.Length - 1;
+             aIndex >= 0 || bIndex >= 0;
+             aIndex--, bIndex--)
+        {
+            var aValue = GetValue(a, aIndex);
+            var bValue = GetValue(b, bIndex);
+
+            (sum, inMemory) = AddBits(aValue, bValue, inMemory);
+            result = string.Concat(sum, result);
+        }
+        
+        if(inMemory is not '0')
+            result = string.Concat(inMemory, result);
+        
+        return result;
+    }
+
+    private char GetValue(string str, int strIndex) =>
+        strIndex < str.Length && strIndex >= 0
+            ? str[strIndex]
+            : '0';
+
+    private (char sum, char inMemory) AddBits(char x, char y, char inMemory)
+    {
+        if (x is '0' && y is '0')
+        {
+            switch (inMemory)
+            {
+                case '0':
+                    return (sum:'0', inMemory:'0');
+                case '1':
+                    return (sum:'1', inMemory:'0');
+            }
+        }
+        
+        if ((x is '0' && y is '1') || (x is '1' && y is '0'))
+        {
+            switch (inMemory)
+            {
+                case '0':
+                    return (sum:'1', inMemory:'0');
+                case '1':
+                    return (sum:'0', inMemory:'1');
+            }
+        }
+        
+        if (x is '1' && y is '1')
+        {
+            switch (inMemory)
+            {
+                case '0':
+                    return (sum:'0', inMemory:'1');
+                case '1':
+                    return (sum:'1', inMemory:'1');
+            }
+        }
+
+        return (sum:'0', inMemory:'0');
+    }
+}
---
+++ tests/LeetCode.UnitTests/Algorithms/Easy/String/AddBinarySolutionTests.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.UnitTests.Algorithms.Easy.String;
+
+public sealed class AddBinarySolutionTests
+{
+    [Theory]
+    [InlineData("11", "1", "100")]
+    [InlineData("11", "11", "110")]
+    [InlineData("1010", "1011", "10101")]
+    public void Given_some_set_of_input_parameters_When_call_TwoSum_Then_should_return_correct_solutions(string a, string b, string expectedSolution)
+    {
+        // Arrange
+        var alg = new AddBinarySolution();
+
+        // Act
+        var actualSolution = alg.AddBinary(a, b);
+
+        // Assert
+        actualSolution.Should().Be(expectedSolution);
+    }
+}
