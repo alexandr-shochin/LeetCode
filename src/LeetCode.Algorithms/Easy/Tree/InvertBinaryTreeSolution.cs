689ff199c3a31778264a3f7e35d20fcff00eb8d7
Александр Шочин <alexandr.shochin@gmail.com>
2023-04-28 16:59:09 +0300

    Algorithms. Easy. 226. Invert Binary Tree - Solved

---
+++ src/LeetCode.Algorithms/Easy/Tree/InvertBinaryTreeSolution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms.Easy.Tree;
+
+/// <summary>226. Invert Binary Tree</summary>
+/// <remarks>https://leetcode.com/problems/invert-binary-tree/</remarks>
+public sealed class InvertBinaryTreeSolution
+{
+    public TreeNode? InvertTree(TreeNode? root)
+    {
+        if (root is null)
+            return null;
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue is not { Count: 0 })
+        {
+            var currentNode = queue.Dequeue();
+
+            (currentNode.left, currentNode.right) = (currentNode.right, currentNode.left); // swap
+            
+            if(currentNode.right is not null)
+                queue.Enqueue(currentNode.right);
+            
+            if(currentNode.left is not null)
+                queue.Enqueue(currentNode.left);
+        }
+
+        return root;
+    }
+}
---
+++ tests/LeetCode.UnitTests/Algorithms/Easy/Tree/InvertBinaryTreeSolutionTests.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.UnitTests.Algorithms.Easy.Tree;
+
+public sealed class InvertBinaryTreeSolutionTests
+{
+    [Fact]
+    public void Given_some_set_of_input_parameters_When_call_MaxDepth_Then_should_return_correct_solutions()
+    {
+        // Arrange
+        var alg = new InvertBinaryTreeSolution();
+
+        // Act
+        var root = new TreeNode(
+            4,
+            new TreeNode(2, 
+                new TreeNode(1),
+                new TreeNode(3)),
+            new TreeNode(7, 
+                new TreeNode(6),
+                new TreeNode(9)));
+        var actualSolution = alg.InvertTree(root);
+
+        // Assert
+        actualSolution!.val.Should().Be(4);
+        
+        actualSolution.left!.val.Should().Be(7);
+        actualSolution.left.left!.val.Should().Be(9);
+        actualSolution.left.right!.val.Should().Be(6);
+        
+        actualSolution.right!.val.Should().Be(2);
+        actualSolution.right.left!.val.Should().Be(3);
+        actualSolution.right.right!.val.Should().Be(1);
+    }
+}
